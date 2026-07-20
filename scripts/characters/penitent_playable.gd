1f88fa0f4ed392c8d71af8befd4e35271ff214b7
D3MISEarch <drivr520@gmail.com>
2026-07-20 13:25:20 -0700

    Compose Ashen Procession into the playable Penitent

--- scripts/characters/penitent_playable.gd
+++ scripts/characters/penitent_playable.gd
@@ -2,8 +2,10 @@ extends "res://scripts/characters/penitent_character.gd"
 class_name PenitentPlayable
 
 const MARTYRS_CHAIN_CONTROLLER_SCRIPT = preload("res://scripts/characters/penitent/martyrs_chain_controller.gd")
+const ASHEN_PROCESSION_CONTROLLER_SCRIPT = preload("res://scripts/characters/penitent/ashen_procession_controller.gd")
 
 var martyrs_chain_controller: MartyrsChainController
+var ashen_procession_controller: AshenProcessionController
 
 
 func _ready() -> void:
@@ -12,3 +14,8 @@ func _ready() -> void:
 	martyrs_chain_controller.name = "MartyrsChainController"
 	add_child(martyrs_chain_controller)
 	martyrs_chain_controller.bind_to(self)
+
+	ashen_procession_controller = ASHEN_PROCESSION_CONTROLLER_SCRIPT.new() as AshenProcessionController
+	ashen_procession_controller.name = "AshenProcessionController"
+	add_child(ashen_procession_controller)
+	ashen_procession_controller.bind_to(self)
