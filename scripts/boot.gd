fba442b33a85d278ab022af38bbb7c9efc9a3025
D3MISEarch <drivr520@gmail.com>
2026-07-20 11:52:40 -0700

    Add class selection boot flow

---
+++ scripts/boot.gd
@@ -0,0 +1,51 @@
+extends Node
+
+const CHARACTER_FACTORY = preload("res://scripts/core/character_factory.gd")
+const CLASS_SELECTION_SCRIPT = preload("res://scripts/ui/class_selection_screen.gd")
+const GAMEPLAY_SCENE = preload("res://gameplay.tscn")
+
+var class_selection: ClassSelectionScreen
+var gameplay_root: Node3D
+
+
+func _ready() -> void:
+	var command_line_class := _get_command_line_class()
+	if CHARACTER_FACTORY.has_class(command_line_class):
+		call_deferred("_launch_gameplay", command_line_class)
+		return
+	_show_class_selection()
+
+
+func _show_class_selection() -> void:
+	if is_instance_valid(class_selection):
+		return
+	class_selection = CLASS_SELECTION_SCRIPT.new()
+	class_selection.name = "ClassSelectionScreen"
+	class_selection.class_confirmed.connect(_on_class_confirmed)
+	add_child(class_selection)
+
+
+func _on_class_confirmed(class_id: String) -> void:
+	if not CHARACTER_FACTORY.has_class(class_id):
+		push_warning("Class-selection screen requested an unavailable class: %s" % class_id)
+		return
+	_launch_gameplay(class_id)
+
+
+func _launch_gameplay(class_id: String) -> void:
+	if is_instance_valid(gameplay_root):
+		return
+	if is_instance_valid(class_selection):
+		class_selection.queue_free()
+		class_selection = null
+
+	gameplay_root = GAMEPLAY_SCENE.instantiate()
+	gameplay_root.requested_class_id = class_id
+	add_child(gameplay_root)
+
+
+func _get_command_line_class() -> String:
+	for argument in OS.get_cmdline_user_args():
+		if argument.begins_with("--class="):
+			return argument.trim_prefix("--class=")
+	return ""
