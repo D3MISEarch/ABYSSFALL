5e110c4306dc560b28fd2cbf9924281c36f58b30
D3MISEarch <drivr520@gmail.com>
2026-07-21 14:01:47 -0700

    Add ability rejection runtime event

--- scripts/runtime/runtime_event_bus.gd
+++ scripts/runtime/runtime_event_bus.gd
@@ -4,6 +4,7 @@ extends Node
 signal build_loaded(build_id: String)
 signal runtime_state_changed(build_id: String, reason: StringName)
 signal ability_cast(build_id: String, ability_id: StringName)
+signal ability_rejected(build_id: String, ability_id: StringName, reason: StringName)
 signal item_equipped(build_id: String, slot_id: StringName, item_instance_id: String)
 signal enemy_killed(enemy_id: StringName, killer_build_id: String)
 signal experience_gained(build_id: String, amount: int)
